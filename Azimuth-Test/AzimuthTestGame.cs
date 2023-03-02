ad0beb5c1ec91b52a03d078d170085ad7ab4f5d7
James Mills <james.mills@aie.edu.au>
2023-03-02 14:08:21 +1100

    button events

--- Azimuth-Test/AzimuthTestGame.cs
+++ Azimuth-Test/AzimuthTestGame.cs
@@ -9,15 +9,36 @@ namespace Azimuth_Test
 	{
 		private ImageWidget image;
 		private Button button;
-		
+
+		private void OnClickButton()
+		{
+			Console.WriteLine("Hello world!");
+		}
+
 		public override void Load()
 		{
+			int counter = 0;
+			
 			image = new ImageWidget(Vector2.Zero, new Vector2(200, 400), "imagewidget");
 			button = new Button(Vector2.Zero, new Vector2(150, 75), Button.RenderSettings.normal);
 			button.SetDrawLayer(100);
+			button.AddListener(OnClickButton);
+			button.AddListener(() =>
+			{
+				if(counter % 2 == 0)
+				{
+					UIManager.Add(image);
+				}
+				else
+				{
+					UIManager.Remove(image);
+				}
+
+				counter++;
+			});
 			
 			UIManager.Add(button);
-			UIManager.Add(image);
+			//UIManager.Add(image);
 		}
 
 		public override void Unload()
